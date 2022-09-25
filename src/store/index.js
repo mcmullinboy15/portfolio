a99471e6c7a43fe3ccbdffc4a59b3398083de475
Andrew McMullin <mcmullinboy15@gmail.com>
2022-09-25 17:22:14 -0400

    Update text for 'This Portfolio'

--- src/store/index.js
+++ src/store/index.js
@@ -78,7 +78,7 @@ export default new Vuex.Store({
         github: "https://github.com/mcmullinboy15/portfolio",
         title: "This Portfolio",
         description:
-          "Click the github link to see the code for this webpage. I use Vue.js and my frontend framework, with Vuex as my statemanagement system, and Vuetify for design. I use Firebase Hosting to host the built webpage, and FirestoreDB for retrieving/saving any data needed. This is a low-level simple project, so I don't use all the great functionalities of these libraries and frameworks."
+          "Click the github link to see the code for this webpage. I use Vue.js and my frontend framework, with Vuex as my statemanagement system, and Vuetify for design. I use Firebase Hosting to host the built webpage, and FirestoreDB for retrieving/saving any data needed."
       },
       {
         github: "https://github.com/mcmullinboy15/simply_ai",
